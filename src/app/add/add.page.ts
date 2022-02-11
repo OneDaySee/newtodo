5dfd3e0a7773ec7c3b89104bcc7fb8005029c272
OneDaySee <onderbirgungor@gmail.com>
2022-02-11 17:49:56 +0300

    first commit

---
+++ src/app/add/add.page.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  ModalController,
+  ToastController,
+  AlertController,
+} from '@ionic/angular';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.page.html',
+  styleUrls: ['./add.page.scss'],
+})
+export class AddPage implements OnInit {
+  public name: any;
+  public input: any;
+  public url: any = 'http://microwebservice.net/ecodation/17temmuz/onder/';
+  public malzemedata: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    public modalController: ModalController,
+    private http: HttpClient,
+    public toastController: ToastController,
+    public alertController: AlertController
+  ) {}
+
+  ngOnInit() {
+    this.input = this.activatedRoute.snapshot.paramMap.get('input');
+  }
+
+  close() {
+    this.modalController.dismiss({
+      dismissed: true,
+      gidenVeri: this.input + this.malzemedata,
+    });
+  }
+
+  send() {
+    this.close();
+    this.insertdelete();
+  }
+
+  insertdelete() {
+    this.http.get(this.url + 'malzeme.php?malzeme=' + this.input).subscribe((data) => {
+      this.malzemedata = data;
+      console.log('modala malzemeler geldi');
+      });
+  }
+}
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {
+  ModalController,
+  ToastController,
+  AlertController,
+} from '@ionic/angular';
+import { animationFrameScheduler } from 'rxjs';
+import { AddPage } from '../add/add.page';
 
 @Component({
   selector: 'app-folder',
@@ -7,12 +15,83 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+  public folder: any;
+  public addinput: any;
+  public yazi: any;
+  userData: any;
+  id: string;
+  public url: any = 'http://microwebservice.net/ecodation/17temmuz/onder/';
+  public malzemedata: any;
+  importmalzeme: any;
+  public input: any;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  filtertext = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    public modalController: ModalController,
+    private http: HttpClient,
+    public toastController: ToastController,
+    public alertController: AlertController
+  ) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    // this.input = this.activatedRoute.snapshot.paramMap.get('input');
+    // this.addinput = JSON.parse(localStorage.getItem('userJSON'));
+    this.http.get(this.url + 'malzeme.php?malzeme2=?').subscribe((data) => {
+      this.importmalzeme = data;
+      console.log('anasayfaya giriste geldi');
+    });
   }
 
+  // doRefresh(event) {
+  //   console.log('Begin async operation');
+
+  //   setTimeout(() => {
+  //     console.log('Async operation has ended');
+  //     event.target.complete();
+  //     this.http.get(this.url+'malzeme.php?malzeme2=?').subscribe(data=>{this.importmalzeme = data;
+  //       console.log("geldi")});
+  //   }, 2000);
+  // }
+
+  async add() {
+    const modal = await this.modalController.create({
+      component: AddPage,
+      cssClass: 'my-custom-class',
+    });
+
+    modal.onDidDismiss().then((gelenVeri) => {
+      // console.log(gelenVeri.data.gidenVeri)
+      this.addinput = gelenVeri.data.gidenVeri;
+      this.http.get(this.url + 'malzeme.php?malzeme2=?').subscribe((data) => {
+        this.importmalzeme = data;
+        console.log('anasayfaya malzemeler geldi');
+      });
+      // localStorage.setItem('userJSON',JSON.stringify(this.addinput));
+    });
+
+    return await modal.present();
+  }
+
+  delete() {
+    // localStorage.removeItem('userJSON')
+    this.addinput = '';
+    this.http.get(this.url + 'malzeme.php?malzeme3=').subscribe((data) => {
+      this.importmalzeme = data;
+      console.log('sildi');
+    });
+  }
+
+  // inputfolder(){
+
+  //   this.http.get(this.url+'malzeme.php?malzeme=').subscribe(data=>{this.importmalzeme = data;
+  //     console.log("malzemeler geldi")});
+
+  // }
+
+  addToFav(mal: string) {
+    console.log(mal);
+  }
 }
